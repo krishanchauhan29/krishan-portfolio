649fe9b6bf7e2bf23ffec2301e834f9cf5671a4c
Krishan Kumar Chauhan <krrishnachauhan29@gmail.com>
2026-06-11 07:27:57 +0530

    Fix unused import in Certifications

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { FiAward, FiExternalLink } from 'react-icons/fi';
+import { FiAward } from 'react-icons/fi';
 import { certifications } from '../../portfolioData';
 import './Certifications.css';
 
